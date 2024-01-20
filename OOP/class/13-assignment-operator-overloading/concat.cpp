e43b1aa1a0200d08d5107a80cc0de04b5df9dac5
intropercib <intropercib@gmail.com>
2024-01-20 19:54:42 +0545

    modified

--- OOP/class/13-assignment-operator-overloading/concat.cpp
+++ OOP/class/13-assignment-operator-overloading/concat.cpp
@@ -1,48 +1,53 @@
 #include <iostream>
-#include<cstring>
+#include <cstring>
 
 using namespace std;
 
-class concat{
+class concat
+{
     char *str;
-    public:
-        concat(){
-            str = new char[1];
-            str[0]='\0';
-        }
-        concat (char *nam){
-            int len = strlen(nam);
-            str = new char[len + 1];
-            strcpy(str,nam);
-        }
-        void show(){
-            cout << "name is: " << str << endl;
-        }
-        ~concat(){}
-        concat operator +(concat obj){
-            int len = strlen(obj.str) + strlen(this->str);
-            char *temp = new char[len + 1];
-            strcpy(temp, this->str);
-            strcat(temp,obj.str);
-            str = new char[len + 1];
-            strcpy(str,temp);
-            return *this;
-        }
-        // friend concat operator +(concat obj1, concat obj2){
-        //     int len = strlen(obj1.str) + strlen(obj2.str);
-        //     char *temp = new char[len + 1];
-        //     strcpy(temp, obj2.str);
-        //     strcat(temp,obj1.str);
-        //     obj2.str = new char[len + 1];
-        //     strcpy(obj2.str,temp);
-        //     return obj2;
-        // }
+
+public:
+    concat()
+    {
+        str = new char[1];
+        str[0] = '\0';
+    }
+    concat(char *nam)
+    {
+        int len = strlen(nam);
+        str = new char[len + 1];
+        strcpy(str, nam);
+    }
+    void show()
+    {
+        cout << "name is: " << str << endl;
+    }
+    ~concat() {}
+    concat operator+(concat obj)
+    {
+        int len = strlen(obj.str) + strlen(this->str);
+        char *temp = new char[len + 1];
+        strcpy(temp, this->str);
+        strcat(temp, obj.str);
+        str = new char[len + 1];
+        strcpy(str, temp);
+        return *this;
+    }
+    // friend concat operator +(concat obj1, concat obj2){
+    //     int len = strlen(obj1.str) + strlen(obj2.str);
+    //     char *temp = new char[len + 1];
+    //     strcpy(temp, obj2.str);
+    //     strcat(temp,obj1.str);
+    //     obj2.str = new char[len + 1];
+    //     strcpy(obj2.str,temp);
+    //     return obj2;
+    // }
 };
 
-
-int main(){
-    concat person1("sita"), person2("ram"),person3;
+int main()
+{
+    concat person1("sita"), person2("ram"), person3;
     person3 = person1 + person2;
     person3.show();
-
 }
